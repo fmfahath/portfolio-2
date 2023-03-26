404c0d57a7e1ec072c1e8350a68c9734a9a5f0cf
fmfahath <fmfahath.dev@gmail.com>
2023-03-26 07:30:19 +0530

    Navbar links added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,24 @@ import "./navbar.css"
 const Navbar = () => {
   return (
     <div className='header'>
+        {/* logo */}
         <Link to='/'>Portfolio</Link>
+
+        {/* Navbar */}
+        <ul>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/project'>Projects</Link>
+            </li>
+            <li>
+                <Link to='/about'>About</Link>
+            </li>
+            <li>
+                <Link to='/contact'>Contact</Link>
+            </li>
+        </ul>
     </div>
   )
 }
