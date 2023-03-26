522712993f60d980825eabd16ef3f17560561da1
fmfahath <fmfahath.dev@gmail.com>
2023-03-26 14:26:29 +0530

    HeroImg - 1 (image, text content, buttons added)

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './heroImg.css'
-
+import heroImg from '../assets/hero-img-1.jpg'
+import { Link } from 'react-router-dom'
 
 const HeroImg = () => {
     return (
-      
+        <div className='hero'>
+            <div className='mask'>
+                <img src={heroImg} alt='hero-img'></img>
+            </div>
+            <div className='hero-content'>
+                <p>HI,I'M A FREEELANCER</p>
+                <h1>React Developer</h1>
+                <div className='hero-content-btn'>
+                    <Link className='btn' to='/project'>Projects</Link>
+                    <Link className='btn btn-light' to='/contact'>Contact</Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
