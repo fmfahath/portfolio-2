d01a14cacb592393472c709a801edaae0e1a7c5c
fmfahath <fmfahath.dev@gmail.com>
2023-03-26 07:58:45 +0530

    navbar css - 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,30 @@ import { Link } from 'react-router-dom'
 import "./navbar.css"
 
 const Navbar = () => {
-  return (
-    <div className='header'>
-        {/* logo */}
-        <Link to='/'>Portfolio</Link>
+    return (
+        <div className='header'>
+            {/* logo */}
+            <div className='nav-logo'>
+                <Link to='/'>Portfolio</Link>
+            </div>
 
-        {/* Navbar */}
-        <ul>
-            <li>
-                <Link to='/'>Home</Link>
-            </li>
-            <li>
-                <Link to='/project'>Projects</Link>
-            </li>
-            <li>
-                <Link to='/about'>About</Link>
-            </li>
-            <li>
-                <Link to='/contact'>Contact</Link>
-            </li>
-        </ul>
-    </div>
-  )
+            {/* Navbar */}
+            <ul className='nav-menu'>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/project'>Projects</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar
