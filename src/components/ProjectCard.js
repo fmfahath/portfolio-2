e87dcaeeaf5b3d6e48adbaa933d2a932ae8646da
fmfahath <fmfahath.dev@gmail.com>
2023-03-28 23:06:07 +0530

    ProjectCard - 2 (css added)

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,53 @@
 import React from 'react'
 import './projectCard.css'
+import ProjectImg1 from '../assets/project-1.jpg'
+import ProjectImg2 from '../assets/project-2.jpg'
+import ProjectImg3 from '../assets/project-3.jpg'
+import { NavLink } from 'react-router-dom'
 
 const ProjectCard = () => {
   return (
-    <div>ProjectCard</div>
+    <div className='project-card-container'>
+        <div className='project-card'>
+            <div className='project-card-img'>
+                <img src={ProjectImg1}></img>
+            </div>
+            <div className='project-card-details'>
+                <h3>Project heading</h3>
+                <p>some text some textsome</p>
+            </div>
+            <div className='project-card-btn'>
+                <NavLink to='url.com' className='btn'>View</NavLink>
+                <NavLink to='url.com' className='btn'>Source</NavLink>
+            </div>
+        </div>
+        <div className='project-card'>
+            <div className='project-card-img'>
+                <img src={ProjectImg2}></img>
+            </div>
+            <div className='project-card-details'>
+                <h3>Project heading</h3>
+                <p>some text some textsome</p>
+            </div>
+            <div className='project-card-btn'>
+                <NavLink to='url.com' className='btn'>View</NavLink>
+                <NavLink to='url.com' className='btn'>Source</NavLink>
+            </div>
+        </div>
+        <div className='project-card'>
+            <div className='project-card-img'>
+                <img src={ProjectImg3}></img>
+            </div>
+            <div className='project-card-details'>
+                <h3>Project heading</h3>
+                <p>some text some textsome</p>
+            </div>
+            <div className='project-card-btn'>
+                <NavLink to='url.com' className='btn'>View</NavLink>
+                <NavLink to='url.com' className='btn'>Source</NavLink>
+            </div>
+        </div>
+    </div>
   )
 }
 
