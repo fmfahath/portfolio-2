ae52bff023be19a634a36873b2d44a66cfb5df8c
fmfahath <fmfahath.dev@gmail.com>
2023-03-28 13:25:20 +0530

    HeroTemplate.js, herotemplate.css created

---
+++ src/components/HeroTemplate.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './heroTemplate.css'
+
+const HeroTemplate = () => {
+  return (
+    <div>HeroTemplate</div>
+  )
+}
+
+export default HeroTemplate
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import HeroTemplate from '../components/HeroTemplate'
 import Navbar from '../components/Navbar'
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
+      <HeroTemplate/>
       <Footer/>
     </div>
   )
