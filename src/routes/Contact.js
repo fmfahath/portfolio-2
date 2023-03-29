1f4e3152f8727c23982c4fe7cdb47cb1c5dbd1fa
fmfahath <fmfahath.dev@gmail.com>
2023-03-29 20:24:41 +0530

    Form - 1 (Form.js, form.css added / rendered on Contac.js)

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import HeroTemplate from '../components/HeroTemplate'
+import Form from '../components/Form'
+
 
 
 const Contact = () => {
+
+  const heading = 'CONTACT';
+  const text = "Feel Free to Contact Me!";
+
   return (
     <div>
       <Navbar/>
+      <HeroTemplate heading={heading} text={text}/>
+      <Form/>
       <Footer/>
     </div>
   )
