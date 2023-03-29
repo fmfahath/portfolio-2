91ff0c3707da70828f663a43f91fece240dcdf90
fmfahath <fmfahath.dev@gmail.com>
2023-03-29 20:29:24 +0530

    Form - 2 (Form created)

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,17 @@ import './form.css'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className='form'>
+        <label>Your Name</label>
+        <input type='text'></input>
+        <label>Emil</label>
+        <input type='email'></input>
+        <label>Subject</label>
+        <input type='text'></input>
+        <label>Message</label>
+        <textarea rows='6' placeholder='Type your message here..'></textarea>
+        <button className='btn'>Submit</button>
+    </div>
   )
 }
 
