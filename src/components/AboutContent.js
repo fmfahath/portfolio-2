3150c50a2c64cb35aeb6ecfce929a03d06bd9457
fmfahath <fmfahath.dev@gmail.com>
2023-03-29 19:20:58 +0530

    AboutContent - 2 (left content added)

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -6,7 +6,10 @@ const AboutContent = () => {
   return (
     <div className='about-container'>
         <div className='about-container-left'>
-           
+        <h1>Who Am I?</h1>
+            <p>I am a React front-end developer. I create responsive secure websites for my clients.
+            </p>
+            <Link to='/contact' className='btn'>CONTACT</Link>
         </div>
         <div className='about-container-right'>
 
