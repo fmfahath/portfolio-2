7d421e4f0a8f3a7cdf962a13d16ffe3c614c8b10
fmfahath <fmfahath.dev@gmail.com>
2023-03-29 19:36:57 +0530

    AboutContent - 4 (class names chaned)

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -5,24 +5,26 @@ import img1 from '../assets/img3.jpg'
 import img2 from '../assets/img1.jpg'
 
 const AboutContent = () => {
-  return (
-    <div className='about-container'>
-        <div className='about-container-left'>
-            <h1>Who Am I?</h1>
-            <p>I am a React front-end developer. I create responsive secure websites for my clients.
-            </p>
-            <Link to='/contact' className='btn'>CONTACT</Link>
-        </div>
-        <div className='about-container-right'>
-            <div className='about-container-right-top'>
-                <img src={img1} alt='img1'></img>
+    return (
+        <div className='about-container'>
+            <div className='left'>
+                <h1>Who Am I?</h1>
+                <p>I am a React front-end developer. I create responsive secure websites for my clients.
+                </p>
+                <Link to='/contact' className='btn'>CONTACT</Link>
             </div>
-            <div className='about-container-right-bottom'>
-                <img src={img2} alt='img2'></img>
+            <div className='right'>
+                <div className='img-container'>
+                    <div className='right-top'>
+                        <img className='img' src={img1} alt='img1'></img>
+                    </div>
+                    <div className='right-bottom'>
+                        <img className='img' src={img2} alt='img2'></img>
+                    </div>
+                </div>
             </div>
         </div>
-</div>
-  )
+    )
 }
 
 export default AboutContent
